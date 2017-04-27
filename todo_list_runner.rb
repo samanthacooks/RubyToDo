4082ffb5f2dae483cff676de7de49b454081545e
Samantha T Cooks <samtcooks@gmail.com>
2017-04-27 11:34:59 -0400

    Update format of printed list of todo tasks

--- todo_list_runner.rb
+++ todo_list_runner.rb
@@ -1,6 +1,5 @@
 require 'csv'
 require 'pry'
-require_relative 'list_parser'
 require_relative 'task'
 require_relative 'todo_list'
 
@@ -9,4 +8,6 @@ require_relative 'todo_list'
 today = ToDoList.new
 my_list = today.load_todo_list('todo_list_data.csv')
 
-p my_list
+
+listed = my_list.each {|task| puts "- #{task.item_description}"}
+
